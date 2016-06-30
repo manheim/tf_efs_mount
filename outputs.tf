68ca39354c9f6011c322c1128de87b4b78642502
Jonathan Niesen <jon.niesen@gmail.com>
2016-06-30 07:10:57 -0400

    feat(outputs): add the attributes

---
+++ outputs.tf
@@ -0,0 +1,23 @@
+output "name" {
+  value = "${var.name}"
+}
+
+output "file_system_id" {
+  value = "${aws_efs_file_systemd.efs.id}"
+}
+
+output "mount_target_ids" {
+  value = "${join(",", aws_efs_mount_target.efs.*.id}"
+}
+
+output "mount_target_interface_ids" {
+  value = "${join(",", aws_efs_mount_target.efs.*.network_interface_id)}"
+}
+
+output "ec2_security_group_id" {
+  value = "${aws_security_group.ec2.id}"
+}
+
+output "mnt_security_group_id" {
+  value = "${aws_security_group.mnt.id}"
+}
